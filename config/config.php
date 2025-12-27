66caf2325cb33ff81457771c3f1718d4b70187b8
onanymouse <50264820+onanymouse@users.noreply.github.com>
2025-12-27 23:46:18 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,11 +3,12 @@
 // Ganti URL ini sesuai alamat web kamu.
 // Jika di localhost: http://localhost/isp_billing/
 // Jika di hosting: https://namadomainkamu.com/
-define('BASE_URL', 'https://dvd.awgnet.biz.id/'); 
+define('BASE_URL', '#'); 
 
 // Nama Aplikasi
 define('APP_NAME','DVD.NET PRO');
 
 // Setting Zona Waktu Indonesia
 date_default_timezone_set('Asia/Jakarta');
+
 ?>
